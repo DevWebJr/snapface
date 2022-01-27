350f7ec46286927c670982ea62a7ba77eddc408b
Dev Web Junior <50543732+DevWebJr@users.noreply.github.com>
2022-01-27 19:17:34 +0100

    add some snapfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,16 +24,24 @@ export class AppComponent implements OnInit{
         description: "Une vieille connaissance",
         imageUrl: 'https://images.bfmtv.com/dPOvIMDQ4ZinodI6FOWmFSWVtlQ=/2x186:1330x933/640x0/images/Chucky-1074231.jpg',
         createdDate: new Date(),
-        snaps: 500,
+        snaps: 510,
       },
       {
         title: "Freddy Krueger",
         description: "Un gars complètement cramé",
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Freddy-freddy-krueger-33746737-500-614.jpg',
         createdDate: new Date(),
-        snaps: 600,
+        snaps: 455,
         location: 'Elm Street'
-      }
+      },
+      {
+        title: "Jason Voorhees",
+        description: "Un mec discret",
+        imageUrl: 'https://store-images.s-microsoft.com/image/apps.63438.71216420060121058.ce3ddcea-6fff-4e2d-b250-569568f21b47.8242fee4-de32-4a61-9619-9dc68ce698b5?q=90&w=480&h=270',
+        createdDate: new Date(),
+        snaps: 500,
+        location: 'Crystal Lake'
+      },
     ];
   }
 
